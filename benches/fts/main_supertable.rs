e968f155ea306988455f8e873464786733443117
Pranav Joglekar <pranav2000joglekar@gmail.com>
2026-05-21 14:14:50 +0530

    benchmarks: allow running superfile and supertable benchmarks seperately
    Before this change, running only superfile benchmarks, also performed
    expensive setup for supertable

---
+++ benches/fts/main_supertable.rs
@@ -0,0 +1,21 @@
+//! Supertable FTS bench (10M docs). Standalone binary comparing infino vs Tantivy.
+//!
+//! ## Invocation
+//!
+//! ```text
+//! cargo bench --bench fts-supertable                            # run all supertable FTS
+//! cargo bench --bench fts-supertable -- <filter>               # criterion regex filter
+//! INFINO_BENCH_FULL=1 cargo bench --bench fts-supertable        # alternative syntax for 10M-doc scale
+//! INFINO_BENCH_UPDATE_README=1 cargo bench --bench fts-supertable
+//! ```
+
+#[path = "../utils/corpus.rs"]
+mod corpus;
+
+#[path = "../utils/markdown.rs"]
+mod markdown;
+
+#[path = "supertable.rs"]
+mod supertable;
+
+criterion::criterion_main!(supertable::benches);
